df1a4b2a32364f6c43c72ce909d248aa589598a3
Anurag@18 <mohitanurag1818@gmail.com>
2026-06-27 12:12:59 +0530

    Fix Login imports

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAppContext } from '../context/AppContext'
-import { toast } from 'react-toast'
+import { toast } from 'react-hot-toast'
 const Login = () => {
 
 
